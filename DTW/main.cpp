ce70d4e5beefe16ccd98695c6a3d44d904ec2fda
LC <602284158@qq.com>
2015-05-06 10:52:32 +0800

    Initial Commit

---
+++ DTW/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DTW
+//
+//  Created by Rinesnow on 15/5/6.
+//  Copyright (c) 2015年 Rinesnow. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
